b10e4c3f77dc62dbb6c257b58e8cc9f44f5d60fb
Jeremy McWilliams <jeremym@lclark.edu>
2022-05-04 11:44:26 -0700

    initial code

---
+++ searchwidget.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+  var primoBase = 'https://alliance-lclark.primo.exlibrisgroup.com/discovery/search?';
+  var wcBase="https://watzek.on.worldcat.org/search?";
+  var boleyWcBase = "https://lewisclarklaw.on.worldcat.org/search?";
+
+  $(".swSubmit").click(function(){
+
+    var tab=$(this).attr("id");
+    var q=$(this).parent("div").siblings("input[name='q']").val();
+    var query = q.replace(",", " ");
+    switch(tab){
+
+      case "primoTab":
+
+        var kw="any,contains,"+query
+        var opts = { "institution":"01ALLIANCE_LCC","vid":"01ALLIANCE_LCC:LCC","query":kw, "search_scope":"EVERYTHING", "lang":"en_US" };
+        var getParams = jQuery.param(opts);
+        var targetUrl=primoBase+getParams;
+        window.location.replace(targetUrl);
+      break;
+
+      case "booksTab":
+
+        var scope=$("input[name='scope']:checked").val();
+        var idx=$("select[name='index']").children("option:selected"). val();
+        if(scope=="worldcat"){
+          var wcidx={"any":"kw", "title":"ti","creator":"au","sub":"su"}
+          var i=wcidx[idx];
+          var qString=i+":"+query;
+          var opts={"queryString":qString}
+          var lib=$(".searchwidget").attr("id");
+          var getParams = jQuery.param(opts);
+          if(lib=="watzek"){
+            var targetUrl=wcBase+getParams;
+            window.location.replace(targetUrl);
+
+          }
+          if(lib=="boley"){
+            var targetUrl=boleyWcBase+getParams;
+            window.location.replace(targetUrl);
+
+          }
+
+        }
+        else{
+          var bookQ=idx+",contains,"+query
+          var opts = { "institution":"01ALLIANCE_LCC","vid":"01ALLIANCE_LCC:LCC","query":bookQ, "search_scope":scope, "lang":"en_US" };
+          var getParams = jQuery.param(opts);
+          var targetUrl=primoBase+getParams;
+          window.location.replace(targetUrl);
+        }
+
+      break;
+
+      case "eonlyTab":
+        var kw="any,contains,"+query
+        var opts = { "institution":"01ALLIANCE_LCC","vid":"01ALLIANCE_LCC:LCC","query":kw, "search_scope":"EResources", "lang":"en_US" };
+        var getParams = jQuery.param(opts);
+        var targetUrl=primoBase+getParams;
+          window.location.replace(targetUrl);
+
+      break;
+
+
+      case "articlesTab":
+        var kw="any,contains,"+query
+        var opts = { "institution":"01ALLIANCE_LCC","vid":"01ALLIANCE_LCC:LCC","query":kw, "search_scope":"EVERYTHING", "lang":"en_US" };
+        var getParams = jQuery.param(opts);
+        var targetUrl=primoBase+getParams+"&mfacet=rtype,include,articles,1&mfacet=rtype,include,newspaper_articles,1";
+        window.location.replace(targetUrl);
+      break;
+
+      case "journalsTab":
+        var kw="title,contains,"+query
+        var opts = { "institution":"01ALLIANCE_LCC","vid":"01ALLIANCE_LCC:LCC","query":kw, "search_scope":"MyInstitution", "lang":"en_US" };
+        var getParams = jQuery.param(opts);
+        var targetUrl=primoBase+getParams+"&mfacet=rtype,include,journals,1&mfacet=rtype,include,newspapers,1";
+        window.location.replace(targetUrl);
+      break;
+
+    }
+
+    return false;
+
+  });
+
+});
